a0d27506eab1af15519bc274293a4f1585e701d4
wedul <rokking2@gmail.com>
2019-05-21 23:08:39 +0900

    첫번재 커밋

---
+++ src/main/java/com/wedul/wedul_timeline/WedulTimelineApplication.java
@@ -0,0 +1,13 @@
+package com.wedul.wedul_timeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WedulTimelineApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WedulTimelineApplication.class, args);
+  }
+
+}
