37461bcd1e0b004d94a5c1e8317ee1be2f3da553
we dul <wedul.chul@gmail.com>
2019-07-12 23:26:31 +0900

    watcher 데몬 추가

---
+++ dev-tool/watcher.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while [ true ]; do
+
+sleep 10
+processCnt=`ps -ef | grep 'java -jar' | wc -l`
+echo $processCnt
+
+if [ $processCnt -lt 2 ]; then
+echo "restart java module"
+cd /var/services/homes/wedul/project
+nohup java -jar -Dspring.profiles.active=dev api-1.0.0.war &
+fi
+
+done
