9fd78451640f5026806dfe4ea59bb1fb59e18153
we dul <wedul.chul@gmail.com>
2019-09-16 21:47:39 +0900

    nohup 추가

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 ps aux | grep -i 'java -jar' | awk '{print $2}' | xargs kill -9
-java -jar -Dspring.profiles.active=production /var/lib/jenkins/workspace/timeline/batch/build/libs/batch-1.0.0.war &
-java -jar -Dspring.profiles.active=production /var/lib/jenkins/workspace/timeline/api/build/libs/api-1.0.0.war &
+nohup java -jar -Dspring.profiles.active=production /var/lib/jenkins/workspace/timeline/batch/build/libs/batch-1.0.0.war &
+nohup java -jar -Dspring.profiles.active=production /var/lib/jenkins/workspace/timeline/api/build/libs/api-1.0.0.war &
